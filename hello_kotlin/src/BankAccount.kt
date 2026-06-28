dbba3c028b9910e2e2b1d320edd64bfc4ae6a852
Sajad Ali Tavakoli <sajadalitavakoli@gmail.com>
2026-06-28 10:21:31 +0430

    add enum class and companion object class

--- hello_kotlin/src/BankAccount.kt
+++ hello_kotlin/src/BankAccount.kt
@@ -1,7 +1,13 @@
 class BankAccount (
     val accountOwner: String,
-    initialBalance: Double = 0.0
+    initialBalance: Double = 0.0,
+    val accountType: AccountType = AccountType.SAVING
 ) {
+    companion object {
+        const val MIN_INITIAL_BALANCE = 0.0
+        const val CURRENCY = "AFN"
+    }
+
     var balance: Double = initialBalance
         private set
 
@@ -16,7 +22,7 @@ class BankAccount (
 
     init {
         require(value = accountOwner.isNotBlank()) { "Account owner must not be blank" }
-        require(value = initialBalance >= 0.0) { "Initial balance can't be negative" }
+        require(value = initialBalance >= MIN_INITIAL_BALANCE) { "Initial balance can't be negative" }
     }
 
     fun deposit(amount: Double): Boolean {
@@ -34,4 +40,18 @@ class BankAccount (
         }
         return false
     }
+
+    fun printAccountType (accountType: AccountType) {
+        when (accountType) {
+            AccountType.SAVING -> println("Saving account")
+            AccountType.BUSINESS -> println("BUSINESS account")
+            AccountType.CURRENT -> println("Current account")
+        }
+    }
+}
+
+enum class AccountType {
+    SAVING,
+    CURRENT,
+    BUSINESS,
 }
