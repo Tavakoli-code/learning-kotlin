9f53c89fe563a558c3206ebafa84276c8d22e2ab
Sajad Ali Tavakoli <sajadalitavakoli@gmail.com>
2026-06-29 11:29:56 +0430

    add sample conversation data and create a list in UI

--- ComposeTutorial/app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ ComposeTutorial/app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -14,6 +14,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -32,7 +34,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeTutorialTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    MessageCard(Message("Android", "Jetpack Compose"))
+                    Conversation(SampleData.conversationSample)
                 }
             }
         }
@@ -90,4 +92,95 @@ fun PreviewMessageCard() {
             )
         }
     }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation(){
+    ComposeTutorialTheme {
+        Conversation(SampleData.conversationSample)
+    }
+}
+
+/**
+ * SampleData for Jetpack Compose Tutorial
+ */
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Lexi",
+            "Test...Test...Test..."
+        ),
+        Message(
+            "Lexi",
+            """List of Android versions:
+            |Android KitKat (API 19)
+            |Android Lollipop (API 21)
+            |Android Marshmallow (API 23)
+            |Android Nougat (API 24)
+            |Android Oreo (API 26)
+            |Android Pie (API 28)
+            |Android 10 (API 29)
+            |Android 11 (API 30)
+            |Android 12 (API 31)""".trim()
+        ),
+        Message(
+            "Lexi",
+            """I think Kotlin is my favorite programming language.
+            |It's so much fun!""".trim()
+        ),
+        Message(
+            "Lexi",
+            "Searching for alternatives to XML layouts..."
+        ),
+        Message(
+            "Lexi",
+            """Hey, take a look at Jetpack Compose, it's great!
+            |It's the Android's modern toolkit for building native UI.
+            |It simplifies and accelerates UI development on Android.
+            |Less code, powerful tools, and intuitive Kotlin APIs :)""".trim()
+        ),
+        Message(
+            "Lexi",
+            "It's available from API 21+ :)"
+        ),
+        Message(
+            "Lexi",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        Message(
+            "Lexi",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        Message(
+            "Lexi",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        Message(
+            "Lexi",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        Message(
+            "Lexi",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        Message(
+            "Lexi",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            "Lexi",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
 }
