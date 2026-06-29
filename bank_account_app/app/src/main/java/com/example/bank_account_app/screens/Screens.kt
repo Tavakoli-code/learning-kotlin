a552ea285ecb4995fd06275f61f794840ffd1678
Sajad Ali Tavakoli <sajadalitavakoli@gmail.com>
2026-06-29 16:55:36 +0430

    add text fields

--- bank_account_app/app/src/main/java/com/example/bank_account_app/screens/Screens.kt
+++ bank_account_app/app/src/main/java/com/example/bank_account_app/screens/Screens.kt
@@ -1,28 +1,91 @@
 package com.example.bank_account_app.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.Button
+import androidx.compose.material3.Label
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.bank_account_app.R
 
+val AppFontFamily = FontFamily(
+    Font(R.font.open_sans, FontWeight.Normal)
+)
 @Composable
 fun BankAccountScreen(modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier.padding(16.dp)
+        modifier = modifier.padding(16.dp).fillMaxSize()
     ) {
-        Text(text = "Bank Account")
+        Text(
+            text = "Bank Account",
+            style = MaterialTheme.typography.titleLarge,
+            textAlign = TextAlign.Center,
+            fontSize = 30.sp,
+            fontFamily = AppFontFamily,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(Modifier.height(8.dp))
+
         Text(text = "Owner: Ali")
+        Text(text = "Account type: Savings")
         Text(text = "Balance: 100.00 AFN")
 
-        Button(
-            onClick = {
-                println("Deposit clicked")
+        Row(horizontalArrangement = Arrangement.spacedBy(12.dp)) {
+            Column(
+                modifier = Modifier.weight(1F),
+            ) {
+                OutlinedTextField(
+                    state = rememberTextFieldState(),
+                    label = { Text("Withdraw amount")},
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+                Button(
+                    onClick = {
+                        println("Withdraw clicked")
+                    }
+                ) {
+                    Text(text = "Withdraw")
+                }
             }
-        ) {
-            Text(text = "Deposit")
+            Column(
+                modifier = Modifier.weight(1F),
+            ) {
+                OutlinedTextField(
+                    state = rememberTextFieldState(),
+                    label = { Text("Deposit amount") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+                Button(
+                    onClick = {
+                        println("Deposit clicked")
+                    }
+                ) {
+                    Text(text = "Deposit")
+                }
+
+            }
         }
     }
 }
