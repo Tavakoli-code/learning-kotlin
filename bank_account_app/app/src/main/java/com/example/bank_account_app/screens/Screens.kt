79467f23f6d21ca18da6dd4983740fad3d78c4f6
Sajad Ali Tavakoli <sajadalitavakoli@gmail.com>
2026-06-30 09:55:43 +0430

    add result text and change alignment

--- bank_account_app/app/src/main/java/com/example/bank_account_app/screens/Screens.kt
+++ bank_account_app/app/src/main/java/com/example/bank_account_app/screens/Screens.kt
@@ -8,15 +8,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.Button
-import androidx.compose.material3.Label
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.Font
@@ -32,8 +34,14 @@ val AppFontFamily = FontFamily(
 )
 @Composable
 fun BankAccountScreen(modifier: Modifier = Modifier) {
+    var resultMessage by remember {
+        mutableStateOf("Result will appear here")
+    }
+
     Column(
-        modifier = modifier.padding(16.dp).fillMaxSize()
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxSize()
     ) {
         Text(
             text = "Bank Account",
@@ -47,45 +55,60 @@ fun BankAccountScreen(modifier: Modifier = Modifier) {
         Spacer(Modifier.height(8.dp))
 
         Text(text = "Owner: Ali")
-        Text(text = "Account type: Savings")
+        Text(text = "Type: Savings")
         Text(text = "Balance: 100.00 AFN")
 
-        Row(horizontalArrangement = Arrangement.spacedBy(12.dp)) {
-            Column(
-                modifier = Modifier.weight(1F),
-            ) {
-                OutlinedTextField(
-                    state = rememberTextFieldState(),
-                    label = { Text("Withdraw amount")},
-                    modifier = Modifier
-                        .fillMaxWidth()
-                )
-                Button(
-                    onClick = {
-                        println("Withdraw clicked")
-                    }
-                ) {
-                    Text(text = "Withdraw")
-                }
-            }
-            Column(
-                modifier = Modifier.weight(1F),
-            ) {
-                OutlinedTextField(
-                    state = rememberTextFieldState(),
-                    label = { Text("Deposit amount") },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                )
-                Button(
-                    onClick = {
-                        println("Deposit clicked")
-                    }
-                ) {
-                    Text(text = "Deposit")
-                }
+        Spacer(Modifier.height(15.dp))
 
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            OutlinedTextField(
+                state = rememberTextFieldState(),
+                label = { Text("Withdraw amount")},
+                modifier = Modifier.weight(1F)
+            )
+            Button(
+                onClick = {
+                    resultMessage = "Withdraw clicked"
+                },
+                modifier = Modifier
+                    .width(110.dp)
+            ) {
+                Text(text = "Withdraw")
             }
         }
+
+        Spacer(Modifier.height(12.dp))
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            OutlinedTextField(
+                state = rememberTextFieldState(),
+                label = { Text("Deposit amount") },
+                modifier = Modifier.weight(1F)
+            )
+            Button(
+                onClick = {
+                    resultMessage = "Deposit clicked"
+                },
+                modifier = Modifier
+                    .width(110.dp)
+            ) {
+                Text(text = "Deposit")
+            }
+        }
+
+        Spacer(Modifier.height(32.dp))
+        Text(
+            text = resultMessage,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
     }
 }
