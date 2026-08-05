d6429b6576dfead25d0bed20d5e0bb12fb355146
Sajad Ali Tavakoli <sajadalitavakoli@gmail.com>
2026-08-05 12:02:21 +0430

    change sdk from 36 to 37

--- bank_account_app/app/build.gradle.kts
+++ bank_account_app/app/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hiltAndroid)
+
     id("com.google.devtools.ksp")
 }
-
 android {
     namespace = "com.example.bank_account_app"
     compileSdk {
@@ -29,8 +30,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     buildFeatures {
         compose = true
@@ -38,6 +39,8 @@ android {
 }
 
 dependencies {
+    implementation(libs.hiltAndroid)
+    ksp(libs.hiltCompiler)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
